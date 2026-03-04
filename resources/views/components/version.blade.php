a9c679c62d02f0d0075867877af049a0073fee30
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2026-03-04 16:49:49 +0100

    Add link to PIGMIL in version component

--- resources/views/components/version.blade.php
+++ resources/views/components/version.blade.php
@@ -1,1 +1,4 @@
-
+<a {{ $attributes->merge(['class' => 'text-xs cursor-pointer opacity-90 hover:opacity-100 dark:hover:text-white hover:text-black']) }}
+    href="https://pigmil.com" target="_blank">
+    PIGMIL
+</a>
