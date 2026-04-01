6270ba44b7a06c98be1edc371dc4b1f3ed0d919f
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2026-04-02 01:12:17 +0200

    V1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.base')
 @section('body')
-    @parent
-    @if (isSubscribed() || !isCloud())
-        <livewire:layout-popups />
-    @endif
+    @parent 
     <!-- Global search component - included once to prevent keyboard shortcut duplication -->
     <livewire:global-search />
     @auth
