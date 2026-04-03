fb7cf37d2facd5d9a92f28322a8996d9bf34093b
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2026-04-03 15:21:55 +0200

    v1

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -80,7 +80,7 @@
     <div class="flex flex-nowrap items-center lg:pt-6 pt-4 mb-2 pl-2">
         <div class="flex flex-col w-full shrink-2">
             <a href="/" {{ wireNavigate() }} class="text-2xl font-bold tracking-wide dark:text-white hover:opacity-80 transition-opacity">
-                <img src="{{ asset('console_icon.webp') }}" alt="Console" class="h-8 w-auto inline-block border rounded-lg" /> 
+                <img src="{{ asset('console_icon.webp') }}" alt="Console" class="h-8 w-auto inline-block" /> 
             </a>
             <!-- <x-version /> -->
         </div>
